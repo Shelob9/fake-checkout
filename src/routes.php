b52ad58224d76835d463bfbd2c5b778333ac3348
Josh Pollock <jpollock412@gmail.com>
2019-08-05 09:52:45 -0400

    inital commit

---
+++ src/routes.php
@@ -0,0 +1,14 @@
+<?php
+
+use Slim\App;
+use Slim\Http\Request;
+use Slim\Http\Response;
+return function (App $app) {
+    $app->get('/', function (Request $request, Response $response) {
+    	$url = urldecode( $request->getQueryParam('redirect') );
+    	if( ! filter_var($url, FILTER_VALIDATE_URL)) {
+    		throw new Exception( 'Not valid redirect URL' );
+		}
+    	$response->write( sprintf('<a href="%s">%s</a>', $url, 'Return to '. $url) );
+    });
+};
